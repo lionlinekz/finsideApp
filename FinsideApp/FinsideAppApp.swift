a8ad27063a18d3e3ea940136c1ea17e9096e4b4c
Asset Sarsengaliyev <aset.kaz@gmail.com>
2026-03-13 12:13:22 +0500

    Initial Commit

---
+++ FinsideApp/FinsideAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinsideAppApp.swift
+//  FinsideApp
+//
+//  Created by Asset Sarsengaliyev on 13.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct FinsideAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
